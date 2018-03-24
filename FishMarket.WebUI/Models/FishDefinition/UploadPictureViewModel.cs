12653dd711eb6a31313c89526c2b1f9ffdc10988
aburakcinar <aburakcinar@gmail.com>
2018-03-24 23:16:33 +0300

    WebUI da balık listeleme sayfası oluşturuldu. WebUI da resim yükleme sayfası oluşturuldu.

---
+++ FishMarket.WebUI/Models/FishDefinition/UploadPictureViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace FishMarket.WebUI.Models.FishDefinition
+{
+    public class UploadPictureViewModel
+    {
+        public IFormFile PictureFile { get; set; }
+        public int Id { get; set; }
+    }
+}
